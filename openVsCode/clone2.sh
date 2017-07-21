f22dc52f1225c6698d4185cff8a61a220b9fd871
localstepdo <stepdo@microsoft.com>
2017-07-21 12:39:06 -0700

    another usage example

--- openVsCode/clone2.sh
+++ openVsCode/clone2.sh
@@ -3,8 +3,11 @@
 
 # Clones and opens a list of git repositories.
 # Usage examples:
+#
 # bash <fullPath>/clone2.sh remoteUrl1 remoteUrl2 <...> remoteUrlN
+#
 # bash <fullPath>/clone2.sh localDestinationDirectory remoteUrl1 <...> remoteUrlN
+#
 # bash clone2.sh /var/tmp                                  \
 # https://github.com/edx-solutions/xblock-drag-and-drop-v2 \
 # https://github.com/open-craft/xblock-vectordraw          \
@@ -14,6 +17,14 @@
 # https://github.com/edx-solutions/xblock-google-drive     \
 # https://github.com/open-craft/xblock-group-project-v2    \
 # https://github.com/Microsoft/xblock-azure-media-services
+#
+# mkdir -p ~/e/tmp && bash clone2.sh ~/e/tmp \
+# https://github.com/Microsoft/edx-configuration.git  \
+# https://github.com/microsoft/edx-platform.git       \
+# https://github.com/microsoft/edx-theme.git          \
+# https://github.com/Microsoft/oxa_kitchen_sink.git   \
+# https://github.com/Microsoft/oxa-tools.git          \
+#
 
 set -x
 
