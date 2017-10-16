24e712717e908ea1ab811182a88a13c0c8136976
localstepdo <edx_admin@microsoft.com>
2017-10-16 12:59:15 -0700

    binaries from console/terminal

--- install/editors.sh
+++ install/editors.sh
@@ -2,6 +2,7 @@
 
 set -xe
 
+# packages
 sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update
 sudo apt-get install -y ubuntu-make
@@ -11,8 +12,28 @@ set -x
 
 destination_prefix=$HOME/.local/share/umake/ide
 
+# install editors
 umake ide pycharm ${destination_prefix}/pycharm
 umake ide atom    ${destination_prefix}/atom
 umake ide visual-studio-code --accept-license ${destination_prefix}/code
 
 popd
+
+pushd ${destination_prefix}/../bin
+
+# use better binary names
+if ! type charm > /dev/null 2>&1 ; then
+    ln -s jetbrains-pycharm-ce charm
+fi
+if ! type pycharm > /dev/null 2>&1 ; then
+    ln -s jetbrains-pycharm-ce pycharm
+fi
+if ! type code > /dev/null 2>&1 ; then
+    ln -s visual-studio-code code
+fi
+
+popd
+
+echo "PATH variable will be updated upon reboot"
+echo "Allowing 'pycharm, code, and atom' from console terminal"
+echo
